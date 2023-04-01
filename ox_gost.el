da275eaa0cf1623bc329649f6165ee76bd0cfad8
pank-su <frostkslo1@yandex.ru>
2023-04-01 13:52:22 +0300

    Все данные из моего экспорта перенесены

--- ox_gost.el
+++ ox_gost.el
@@ -6,17 +6,24 @@
 (require 'ox-publish)
 (require 'ox-latex)
 
-
 ;; Создаём потомка экспорта ox-latex
-(org-export-define-derived-backend 'ox-gost 'latex
+(org-export-define-derived-backend 'gost 'latex
   ;; Объявляем меню
   :menu-entry '(?g "Export to GOST 7.32" org-gost-export-to-pdf)
-  :filters-alist '(:filter-table . gost-filter-table)
-  ;; TODO: NOT WORKED
-  :options-alist '((:latex-classes nil nil org-gost-classes nil))
+  :filters-alist '((:filter-table . gost-filter-table))
+  :options-alist '(
+		   (:latex-classes nil nil org-gost-classes)
+		   (:latex-toc-command nil nil org-gost-toc-command)
+		   (:latex-title-command nil nil org-gost-title-command)
+		   (:latex-class nil nil "extarticle")
+		   (:latex-minted-options nil nil org-gost-minted-options)
+		   (:latex-default-table-environment nil nil org-gost-default-table-environment)
+		   (:latex-header nil nil org-gost-latex-headers newline)
+		   )
   )
 
 (defun gost-filter-table (text backend info)
+  "Функция исправления таблички"
   (replace-regexp-in-string "Continued on next page" "Продолжение на следующей странице" (replace-regexp-in-string "Continued from previous page" "Продолжение с предыдущей страницы" text))
       (text))
 
@@ -26,7 +33,7 @@
   "Export current buffer to GOST 7.32 file"
   (interactive)
     (let ((outfile (org-export-output-file-name ".tex" subtreep)))
-      (org-export-to-file 'latex outfile
+      (org-export-to-file 'gost outfile
         async subtreep visible-only body-only ext-plist
         #'org-latex-compile)))
 
@@ -93,4 +100,52 @@
 
 (setq org-gost-toc-command "\n\\tableofcontents \\clearpage\n")
 
-;; (defcustom org-gost-listings 'minted)
+;; Надо как-то исправить
+(setq org-gost-latex-headers 
+      "\\usepackage[utf8x]{inputenc}
+\\usepackage{minted}
+\\usepackage[T2A]{fontenc}
+\\usepackage[russian]{babel}
+\\usepackage{tempora}
+\\usepackage{geometry}
+\\geometry{a4paper, left=30mm, top=20mm, bottom=20mm, right=15mm }
+\\usepackage{graphicx}
+\\usepackage{array}
+\\usepackage{tabularx}
+\\usepackage{listings}
+\\usepackage{float}
+\\usepackage{setspace}
+\\usepackage{tabularx}
+\\usepackage{longtable}
+\\usepackage{titlesec}
+\\titleformat*{\\section}{\\large\\bfseries}
+\\titleformat*{\\subsection}{\\normalsize\\bfseries}
+\\titleformat*{\\subsubsection}{\\normalsize\\bfseries}
+\\addto\\captionsrussian{\\renewcommand{\\contentsname}{\\centering \\normalsize СОДЕРЖАНИЕ}}
+\\addtocontents{toc}{\\protect\\thispagestyle{empty}}
+\\usepackage{titletoc}
+\\titlecontents{section}[0pt]{}{\\contentsmargin{0pt} \\thecontentslabel\\enspace}{\\contentsmargin{0pt}}{\\titlerule*[0.5pc]{.}\\contentspage}[]
+\\dottedcontents{subsection}[3.1em]{}{1.5em}{0.5pc}
+\\usepackage{caption}
+\\DeclareCaptionLabelSeparator{custom}{ -- }
+\\captionsetup[figure]{name=Рисунок, labelsep=custom, font={onehalfspacing}, justification=centering}
+\\usepackage{ragged2e}
+\\justifying
+\\setlength\\parindent{1.25cm}
+\\sloppy
+\\usepackage{indentfirst}
+\\usepackage{multirow}
+\\usepackage{lscape}"
+			        )
+
+(setq org-gost-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+	"pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+(setq
+   org-gost-minted-options '(("breaklines" "true")
+	 ("float" "t")
+	 ("breakanywhere" "true")
+	 ("fontsize" "\\footnotesize"))
+    org-gost-default-table-environment "longtable")
